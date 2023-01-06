1b36b4dd06e91b7b2696028e070ae85e01ee8a0c
jarek <js100code@gmail.com>
2023-01-06 20:25:09 +0100

    Add dataType file and hello.hs. Compile hello.hs

---
+++ dataType.hs
@@ -0,0 +1,12 @@
+data RGB = Red | Blue | Green deriving (Eq, Ord, Show, Enum)
+
+
+
+data Person = Person { name :: String, age :: Maybe Int, fav_col :: RGB, address :: [String] }
+joe = Person "Joe" (Just 25) Red ["Durham Cathedral", "Durham"]
+
+main = do 
+ putStrLn "Cześć" 
+
+
+
