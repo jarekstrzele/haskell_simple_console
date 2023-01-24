c0cad27513eb3958dbef63b86df767e7ca0c02e0
jarek <js100code@gmail.com>
2023-01-24 11:45:49 +0100

    Add listIntProduct file with foldl

---
+++ listIntProduct.hs
@@ -0,0 +1,25 @@
+-- prod [2,10,5] -> 100
+-- prod [3,1,2,4] -> 24
+
+prod :: [Int] -> Int 
+prod [x] = x 
+prod (x:xs) = x * prod xs
+
+-- FOLDL
+-- type: (a->b->a)->a->[b]->a
+--      input : foldl (/) 64 [4,2,4]
+--      output : 2.0
+-- 64 / 4 = 16
+-- 16 / 2 = 8
+-- 8  / 4 = 2
+
+-- foldl (*) 1 [7,2,9] -- 1  neutral (initial) element ; in addition 0 is neutral element
+-- 1* 7 = 7
+-- 7 * 2 = 14
+-- 14 * 9 = 126
+
+prod1 :: [Int]->Int
+-- prod1 x = foldl (*) 1 x 
+prod1 = foldl (*) 1
+
+
