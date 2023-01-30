c617fe0e6533defa0dae069d569fec0f8bc14660
jarekPop <js100code@gmail.com>
2023-01-30 11:32:50 +0100

    Add grid, langs in Lib

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -1,13 +1,40 @@
 module Lib
-    ( someFunc,
-      someString
+    ( grid
+    , languages
+    , formatGrid
+    , outputGrid
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn someString
+outputGrid :: [String] -> IO()
+outputGrid grid = putStrLn (formatGrid grid)
 
-someString :: String
-someString = "someString"
+formatGrid :: [String] -> String
+formatGrid = unlines
 
+grid :: [String]
+grid = [ "__C________R___"
+        ,"__SI________U__"
+        ,"__HASKELL____B_"
+        ,"__A__A_____S__Y"
+        ,"__PHP____H_____"
+        ,"____S_LREP_____"
+        ,"____I__M_Y__L__"
+        ,"____L_E__T_O___"
+        ,"_________HB____"
+        ,"_________O_____"
+        ,"________CN_____"
+       ]
 
- 
+languages :: [String]
+languages = 
+  [ "BASIC"
+  , "COBOL"
+  , "CSHARP"
+  , "HASKELL"
+  , "LISP"
+  , "PERL"
+  , "PHP"
+  , "PYTHON"
+  , "RUBY"
+  , "SCHEME"
+  ]
