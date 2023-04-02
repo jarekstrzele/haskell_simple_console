f570edec3212eb80912767a21780e0a5428177a1
jarek <js100code@gmail.com>
2023-04-02 14:11:49 +0200

    Delete Word Game Projects

---
+++ tests.hs
@@ -0,0 +1,11 @@
+data Compass = North | East | South | West 
+    deriving (Eq, Ord, Enum, Show)
+
+-- instance Show Compass where
+--     show North = "North11:l"
+--     show East = "East"
+--     show South = "South"
+--     show West = "West"
+
+
+
