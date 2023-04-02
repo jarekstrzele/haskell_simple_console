16fa30e3bab2e8622c0786e1b2d6b63499a683e6
jarek <js100code@gmail.com>
2023-04-02 16:51:48 +0200

    Delete the Spec content

--- words/test/Spec.hs
+++ words/test/Spec.hs
@@ -1,10 +1,10 @@
-import Test.Hspec
-import Lib
+-- --import Test.Hspec
+-- import Lib
 
--- `do` indicates we're going to sequence things 
+-- -- `do` indicates we're going to sequence things 
 
-main :: IO ()
-main = hspec $ do 
-    describe "How to write a test" $ do 
-        it "Should be able to tun tests" $ do 
-            someString `shouldBe` "someString"
+-- main :: IO ()
+-- main = hspec $ do 
+--     describe "How to write a test" $ do 
+--         it "Should be able to tun tests" $ do 
+--             someString `shouldBe` "someString"
